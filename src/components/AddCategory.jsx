391035a1559cb668dee4f193aaaaa1d6f58e0d3a
henrryrocket <henrryrocket@gmail.com>
2024-09-24 22:49:52 -0300

    fist commit vite test

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = (event) => {
+        setInputValue(event.target.value);
+    }
+    const onSubmit = (event ) => {
+        event.preventDefault();
+        const cleanValue = inputValue.trim();
+
+        if ( cleanValue.length <= 1 ){
+            alert('Input a text valid');
+            return;
+        }
+        onNewCategory( cleanValue );
+        setInputValue('');
+        // setCategories(categories => [inputValue, ...categories])
+    }
+
+    
+  return (
+    <form onSubmit={onSubmit}>
+        <input
+            type="text"
+            placeholder="Search gifs"
+            value={ inputValue }
+            onChange={ onInputChange }
+        />
+    </form>
+  )
+}
