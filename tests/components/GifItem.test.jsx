8d9d2137989b0b8cbe7ca2813a75462c62d2777d
henrryrocket <henrryrocket@gmail.com>
2024-10-03 00:40:10 -0300

    complete tests for GifItem component

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -13,6 +13,15 @@ describe('Tests in <GifItem />', () => {
     test('should show the title and url given', () => { 
         render( <GifItem title={ title } url={ url }/> );
         // screen.debug();
-        expect(screen.getByRole('img').src).toBe( url );
+        // expect(screen.getByRole('img').src).toBe( url );
+        // expect(screen.getByRole('img').alt).toBe( title );
+        const { src, alt } = screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
     });
+
+    test('should show a title', () => { 
+        render( <GifItem title={ title } url={ url }/> );
+        expect(screen.getByText( title )).toBeTruthy();
+    })
 });
--- tests/demo.test.js
+++
@@ -1,5 +0,0 @@
-describe('test', () => {
-    test('should first', () => { 
-        
-     })
-});
