8177ade60be617bdd37a2f85d7e1aeb3c7d5d822
henrryrocket <henrryrocket@gmail.com>
2024-10-03 00:11:24 -0300

    valide title and url given on gifItem component

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,12 +1,18 @@
-import { render, renderHook } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components";
 
 describe('Tests in <GifItem />', () => {
     const title = 'Test-Title';
-    const url = 'https://testPage.com/test.jpg';
+    const url = 'https://testpage.com/test.jpg';
 
     test('should make match with the snapshot', () => { 
-        const { container } = render( <GifItem title='title' url='url'/> );
+        const { container } = render( <GifItem title={ title } url={ url }/> );
         expect(container).toMatchSnapshot();
-    })
+    });
+
+    test('should show the title and url given', () => { 
+        render( <GifItem title={ title } url={ url }/> );
+        // screen.debug();
+        expect(screen.getByRole('img').src).toBe( url );
+    });
 });
