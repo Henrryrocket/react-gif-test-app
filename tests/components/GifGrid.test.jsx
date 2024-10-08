badba512ec42eccdfbab479bcf35c932650be10a
henrryrocket <henrryrocket@gmail.com>
2024-10-08 00:28:49 -0300

    Add test for useFetchGifs custom hook

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,11 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import PropTypes from "prop-types";
 import { GifGrid } from "../../src/components";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs')
 
 
 describe('Tests in <GifGrid/>', () => {
     const category = 'One Punch';
     test('should show loading message first', () => { 
+        //Simulate the firts stage when there are not data yet
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true,
+        });
 
         render(<GifGrid category={category}/>);
         // screen.debug();
@@ -13,6 +21,32 @@ describe('Tests in <GifGrid/>', () => {
         expect(screen.getByText(category));
 
      })
+
+     test('should load images from useFetchGifs', () => { 
+
+        //Sujeto de prueba
+        const gifs = [
+            {
+                id: 'ABC1',
+                title: 'image1',
+                url: 'https://testurl.com/image2.jpg'
+            },
+            {
+                id: 'ABC2',
+                title: 'image2',
+                url: 'https://testurl.com/image2.jpg'
+            }
+        ]
+        
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false,
+        });
+        
+        render(<GifGrid category={category}/>);
+        expect(screen.getAllByRole('img').length).toBe(2);
+
+     });
 });
 
 GifGrid.propTypes = {
